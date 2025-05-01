dd720427db78d74a5d61dae5b2d339c8e5078f4a
snowmuffin <tnt0807@naver.com>
2025-05-01 15:49:11 +0900

    add detail

--- TorchPlugin/CustomWindow.xaml.cs
+++ TorchPlugin/CustomWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using Shared.Plugin;
 using Sandbox.Game.World;
+using Torch.Utils;
 
 namespace TorchPlugin
 {
@@ -14,6 +15,9 @@ namespace TorchPlugin
     public partial class CustomWindow : Window
     {
         private readonly CustomInstance _customInstance;
+
+        [ReflectedGetter(Name = "m_stations", Type = typeof(MyFaction))]
+        private static Func<MyFaction, Dictionary<long, MyStation>> _stations;
 
         public CustomWindow(CustomInstance customInstance)
         {
@@ -54,6 +58,47 @@ namespace TorchPlugin
             }).ToList();
 
             FactionsList.ItemsSource = factions;
+        }
+
+        private void FactionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedFaction = FactionsList.SelectedItem;
+            if (selectedFaction != null)
+            {
+                var factionId = (long)selectedFaction.GetType().GetProperty("FactionId")?.GetValue(selectedFaction);
+                var faction = MySession.Static.Factions.Factions.Values.FirstOrDefault(f => f.FactionId == factionId);
+                if (faction != null)
+                {
+                    MembersGrid.ItemsSource = faction.Members.Select(m => new
+                    {
+                        MemberId = m.Key,
+                        IsLeader = m.Value.IsLeader,
+                        IsFounder = m.Value.IsFounder
+                    }).ToList();
+
+                    if (MySession.Static?.Factions != null)
+                    {
+                        var stations = _stations((MyFaction)faction);
+                        if (stations != null && stations.Count > 0)
+                        {
+                            StationsGrid.ItemsSource = stations.Select(station => new
+                            {
+                                StationId = station.Key,
+                                FactionId = faction.FactionId,
+                                station.Value.PrefabName,
+                                station.Value.Type,
+                                station.Value.Position,
+                                station.Value.IsDeepSpaceStation,
+                                station.Value.IsOnPlanetWithAtmosphere
+                            }).ToList();
+                        }
+                        else
+                        {
+                            StationsGrid.ItemsSource = null;
+                        }
+                    }
+                }
+            }
         }
     }
 }
