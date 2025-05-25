372f6634363d0bd45e159eb21a34570ba577a2d5
snowmuffin <tnt0807@naver.com>
2025-05-25 16:01:09 +0900

    Update CustomWindow.Encounters.cs

--- TorchPlugin/CustomWindow.Encounters.cs
+++ TorchPlugin/CustomWindow.Encounters.cs
@@ -4,17 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Reflection;
 using Sandbox.Game.World;
 using SpaceEngineers.Game.SessionComponents;
 using VRage.Game;
 using Sandbox.Definitions;
 using SpaceEngineers.Game.EntityComponents.GameLogic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace TorchPlugin
 {
     public partial class CustomWindow : Window
     {
+        private ObservableCollection<object> _activeEncounterCollection = new ObservableCollection<object>();
+        private CollectionViewSource _activeEncounterViewSource;
+
         // Encounters 탭 관련 함수 및 이벤트 핸들러
         private void LoadGlobalEncountersData()
         {
@@ -90,6 +96,13 @@ namespace TorchPlugin
             }
             method.Invoke(generatorInstance, new object[] { encounterId });
         }
+        public CustomWindow()
+        {
+            InitializeComponent();
+            _activeEncounterViewSource = new CollectionViewSource { Source = _activeEncounterCollection };
+            _activeEncounterViewSource.GroupDescriptions.Add(new PropertyGroupDescription("EncounterId"));
+            ActiveGlobalEncountersGrid.ItemsSource = _activeEncounterViewSource.View;
+        }
         private void LoadActiveGlobalEncountersData()
         {
             var generatorInstance = MySession.Static.GetComponent<MyGlobalEncountersGenerator>();
@@ -99,33 +112,39 @@ namespace TorchPlugin
                 return;
             }
             var encounterComponentsField = typeof(MyGlobalEncountersGenerator).GetField("m_encounterComponents", BindingFlags.NonPublic | BindingFlags.Instance);
+            var encounterTimerField = typeof(MyGlobalEncountersGenerator).GetField("m_encountersTimer", BindingFlags.NonPublic | BindingFlags.Instance);
+
             if (encounterComponentsField == null)
             {
                 MessageBox.Show("Unable to retrieve m_encounterComponents field.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
             var encounterComponents = encounterComponentsField.GetValue(generatorInstance) as ConcurrentDictionary<long, HashSet<MyGlobalEncounterComponent>>;
+            var encounterTimer = encounterTimerField.GetValue(generatorInstance) as ConcurrentDictionary<long, long>;
             if (encounterComponents == null || encounterComponents.Count == 0)
             {
                 return;
             }
-            var encounterList = new List<object>();
+            _activeEncounterCollection.Clear();
             foreach (var kv in encounterComponents)
             {
                 long encounterId = kv.Key;
+                long timer = 0;
+                if (encounterTimer != null && encounterTimer.TryGetValue(encounterId, out var t))
+                    timer = t;
                 foreach (var comp in kv.Value)
                 {
                     var entity = comp.Entity;
-                    encounterList.Add(new {
+                    _activeEncounterCollection.Add(new {
                         EncounterId = encounterId,
                         comp.SpawnGroupName,
                         EntityId = entity?.EntityId ?? 0,
                         EntityType = entity?.GetType().Name ?? "-",
-                        Position = entity is VRage.Game.Entity.MyEntity ent ? ent.PositionComp?.GetPosition().ToString() : "-"
+                        Position = entity is VRage.Game.Entity.MyEntity ent ? ent.PositionComp?.GetPosition().ToString() : "-",
+                        Timer = timer
                     });
                 }
             }
-            ActiveGlobalEncountersGrid.ItemsSource = encounterList;
         }
         
     }
